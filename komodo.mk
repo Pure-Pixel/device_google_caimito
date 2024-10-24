43488a577e04d8ef6ad2c993fd4f56bfa77749a4
hurtcopain <jacksummers715@gmail.com>
2024-10-24 09:04:18 -0400

    update properties
    Change-Id: Ib28125de4ae02d7ce9fb0af1073b07f89b419ab1

--- komodo.mk
+++ komodo.mk
@@ -76,12 +76,21 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # SetupWizard configuration
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.setupwizard.enterprise_mode=1 \
-    ro.setupwizard.rotation_locked=true \
-    setupwizard.enable_assist_gesture_training=true \
-    setupwizard.theme=glif_v3_light \
+    ro.setupwizard.esim_cid_ignore=00000001 \
     setupwizard.feature.baseline_setupwizard_enabled=true \
+    setupwizard.feature.day_night_mode_enabled=true \
+    setupwizard.feature.enable_gil= \
+    setupwizard.feature.enable_restore_anytime=true \
+    setupwizard.feature.enable_wifi_tracker=true \
+    setupwizard.feature.lifecycle_refactoring=true \
+    setupwizard.feature.notification_refactoring=true \
+    setupwizard.feature.portal_notification=true \
+    setupwizard.feature.provisioning_profile_mode=true \
+    setupwizard.feature.show_digital_warranty=false \
+    setupwizard.feature.show_pai_screen_in_main_flow.carrier1839=false \
+    setupwizard.feature.show_support_link_in_deferred_setup=false \
     setupwizard.feature.skip_button_use_mobile_data.carrier1839=true \
-    setupwizard.feature.show_pai_screen_in_main_flow.carrier1839=false
+    setupwizard.theme=glif_v4_light
 
 # StorageManager configuration
 PRODUCT_PRODUCT_PROPERTIES += \
@@ -98,9 +107,19 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Google Play services configuration
 PRODUCT_PRODUCT_PROPERTIES += \
+    ro.com.google.clientidbase=android-google \
     ro.error.receiver.system.apps=com.google.android.gms \
     ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent
 
+# New charging information
+PRODUCT_PRODUCT_PROPERTIES += \
+    charging_string.apply_lotx=true \
+    charging_string.apply_v2=true
+
+# WFC Activation
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.gwfcactivation.disabled_carriers=1492
+    
 # TextClassifier
 PRODUCT_PACKAGES += \
 	libtextclassifier_annotator_en_model \
@@ -109,13 +128,18 @@ PRODUCT_PACKAGES += \
 	libtextclassifier_lang_id_model
         
 # Use gestures by default
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural;com.google.android.systemui.gxoverlay
+
+# StorageManager configuration
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural
+    ro.storage_manager.show_opt_in=false
 
-# Storage manager
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.storage_manager.enabled=true
-
+# DRM Service
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true \
+    media.mediadrmservice.enable=true
+    
 # Media
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     media.recorder.show_manufacturer_and_model=true
