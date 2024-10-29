df6a000ca3726c917260d5b7fdd27b6b204ecf45
hurtcopain <jacksummers715@gmail.com>
2024-10-29 16:12:40 -0400

    add test flags for GMS
    Change-Id: I1254647cd130401277b14c06e247430d3fec6370

--- komodo.mk
+++ komodo.mk
@@ -19,8 +19,34 @@ $(call inherit-product, device/google/caimito/aosp_komodo.mk)
 # Gapps
 WITH_GMS := true
 $(call inherit-product, vendor/google/gms/products/gms.mk)
+TARGET_SUPPORTS_GOOGLE_CAMERA := true
+TARGET_GOOGLE_CAMERA_LARGE_RAM := true
+TARGET_SUPPORTS_SATELLITE_SOS := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_SUPPORTS_DSE_CHOICE_SCREEN := true
+# Pixel Battery/Adaptive Charging
+#TARGET_SUPPORTS_GOOGLE_BATTERY := true
+#TARGET_SUPPORTS_ADPATIVE_CHARGING := true
+TARGET_SUPPORTS_CLEAR_CALLING := true
 
+TARGET_SUPPORTS_FIR_PROXIMITY_SENSORS := true
+
+TARGET_SUPPORTS_PIXEL_SCREENSHOTS := true
+
+TARGET_SUPPORTS_PIXEL_STUDIO := true
+
+#######
 RELEASE_PIXEL_2024_ENABLED := true
+TARGET_PIXEL_EXPERIENCE_2024 := true
+TARGET_PIXEL_EXPERIENCE_2023 := true
+TARGET_PIXEL_EXPERIENCE_2022 := true
+
+TARGET_SUPPORTS_DREAMLINER := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Boot Animation
 $(call inherit-product, device/google/caimito/bootanimation/bootanimation.mk)
