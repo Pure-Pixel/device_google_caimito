3ff8d6d8f51b32fa7819c4aaaed3766d9ce68ffa
hurtcopain <jacksummers715@gmail.com>
2024-11-01 22:35:10 -0400

    Revert "Revert "switch to hentaiOS gms""
    This reverts commit fff97524567294a5ab8b7fa0b31b309a8702aba2.

--- komodo.mk
+++ komodo.mk
@@ -18,8 +18,7 @@ $(call inherit-product, device/google/caimito/aosp_komodo.mk)
 
 # Gapps
 WITH_GMS := true
-$(call inherit-product-if-exists, vendor/google/gms/config.mk)
-$(call inherit-product-if-exists, vendor/google/pixel/config.mk)
+$(call inherit-product, vendor/google/gms/products/gms.mk)
 $(call inherit-product-if-exists, vendor/google/pixel-additional/config.mk)
 
 # Themed icons for Pixel Launcher
