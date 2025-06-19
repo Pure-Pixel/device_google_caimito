95c048a0bef57f95682aaf70d548bec0762490d8
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2025-06-19 07:38:45 +0000

    strip thermal

--- device-caimito-common.mk
+++ device-caimito-common.mk
@@ -15,26 +15,6 @@
 #
 
 
-# Thermal Config
-ifeq (,$(TARGET_VENDOR_THERMAL_CONFIG_PATH))
-TARGET_VENDOR_THERMAL_CONFIG_PATH := device/google/caimito/thermal
-endif
-
-PRODUCT_COPY_FILES += \
-	$(TARGET_VENDOR_THERMAL_CONFIG_PATH)/thermal_info_config_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json \
-	$(TARGET_VENDOR_THERMAL_CONFIG_PATH)/thermal_info_config_charge_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_charge.json
-
-ifneq (,$(filter $(TARGET_DEVICE),komodo caiman tokay))
-PRODUCT_COPY_FILES += \
-	$(TARGET_VENDOR_THERMAL_CONFIG_PATH)/thermal_info_config_$(TARGET_DEVICE)_proto.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_proto.json \
-	$(TARGET_VENDOR_THERMAL_CONFIG_PATH)/vt_estimation_model_$(TARGET_DEVICE).tflite:$(TARGET_COPY_OUT_VENDOR)/etc/vt_estimation_model.tflite \
-	$(TARGET_VENDOR_THERMAL_CONFIG_PATH)/vt_prediction_lstm_model_$(TARGET_DEVICE).tflite:$(TARGET_COPY_OUT_VENDOR)/etc/vt_prediction_lstm_model.tflite
-        ifneq (,$(filter $(TARGET_BUILD_VARIANT), userdebug eng))
-            PRODUCT_COPY_FILES += \
-            $(TARGET_VENDOR_THERMAL_CONFIG_PATH)/thermal_info_config_$(TARGET_DEVICE)_wingboard.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_wingboard.json
-        endif
-endif
-
 # Telephony Satellite Feature
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.satellite.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.satellite.xml
