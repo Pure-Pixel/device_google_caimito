4a8c89ffd4c77e2e9ef7c9a2d8af1c9e868ae2e6
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2025-06-19 07:13:06 +0000

    strip battery_mitigation

--- device-caimito-common.mk
+++ device-caimito-common.mk
@@ -48,13 +48,3 @@ PRODUCT_COPY_FILES += \
 # Telephony Satellite Feature
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.satellite.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.satellite.xml
-
-# Battery Mitigation Config
-ifneq (,$(filter $(TARGET_DEVICE),komodo caiman tokay))
-ifeq (,$(TARGET_VENDOR_BATTERY_MITIGATION_CONFIG_PATH))
-TARGET_VENDOR_BATTERY_MITIGATION_CONFIG_PATH := device/google/caimito/battery_mitigation
-endif
-
-PRODUCT_COPY_FILES += \
-	$(TARGET_VENDOR_BATTERY_MITIGATION_CONFIG_PATH)/bm_config_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/bm_config.json
-endif
